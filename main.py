f21968c08cb8a3973815becaa264d524c3c155ac
mohamed.hjaji <mohamed.hjaji@gmail.com>
2023-11-30 10:33:55 +0100

    adding dim-red function in main.py

--- main.py
+++ main.py
@@ -12,3 +12,10 @@ def clust(embeddings, k):
     kmeans = KMeans(n_clusters=k, random_state=42)
     predictions = kmeans.fit_predict(embeddings)
     return predictions
+
+
+def dim_red(embeddings, n_components):
+    factor_analysis = FactorAnalysis(n_components=n_components, random_state=42)
+    reduced_embeddings = factor_analysis.fit_transform(embeddings)
+    return reduced_embeddings
+
