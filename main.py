f3d0869453be314c55a86ebaf3fa3320426a7af5
mohamed.hjaji <mohamed.hjaji@gmail.com>
2023-11-30 15:09:14 +0100

    adding umap in main.py

--- main.py
+++ main.py
@@ -1,14 +1,56 @@
-from umap import UMAP
-from sklearn.metrics import accuracy_score
-# Define your custom functions
-def clust(embeddings, k):
-    kmeans = KMeans(n_clusters=k, random_state=42)
-    predictions = kmeans.fit_predict(embeddings)
-    return predictions
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.metrics.cluster import normalized_mutual_info_score, adjusted_rand_score
+from sentence_transformers import SentenceTransformer
+import numpy as np
 
-def dim_red(embeddings, n_components=20):
-    umap_model = UMAP(n_components=n_components, random_state=42)
-    reduced_embeddings = umap_model.fit_transform(embeddin    return reduced_embeddings
+
+def dim_red(mat, p, method):
+    '''
+    Perform dimensionality reduction
+
+    Input:
+    -----
+        mat : NxM list 
+        p : number of dimensions to keep 
+    Output:
+    ------
+        red_mat : NxP list such that p<<m
+    '''
+    if method=='ACP':
+        red_mat = mat[:,:p]
+        
+    elif method=='AFC':
+        red_mat = mat[:,:p]
+        
+    elif method=='UMAP':
+        umap_model = UMAP(n_components=n_components, random_state=42)
+        red_mat = umap_model.fit_transform(embeddings)
+        
+        
+    else:
+        raise Exception("Please select one of the three methods : APC, AFC, UMAP")
+    
+    return red_mat
+
+
+def clust(mat, k):
+    '''
+    Perform clustering
+
+    Input:
+    -----
+        mat : input list 
+        k : number of cluster
+    Output:
+    ------
+        pred : list of predicted labels
+    '''
+    
+    kmeans = KMeans(n_clusters=k, random_state=42)
+    pred = kmeans.fit_predict(red_mat)
+    
+    
+    return pred
 
 # import data
 ng20 = fetch_20newsgroups(subset='test')
@@ -20,14 +62,18 @@ k = len(set(labels))
 model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
 embeddings = model.encode(corpus)
 
-# perform dimentionality reduction
-red_emb = dim_red(embeddings, 20)
+# Perform dimensionality reduction and clustering for each method
+methods = ['ACP', 'AFC', 'UMAP']
+for method in methods:
+    # Perform dimensionality reduction
+    red_emb = dim_red(embeddings, 20, method)
 
-# perform clustering
-pred = clust(red_emb, k)
+    # Perform clustering
+    pred = clust(red_emb, k)
 
-# evaluate clustering results
-nmi_score = normalized_mutual_info_score(pred,labels)
-ari_score = adjusted_rand_score(pred,labels)
-accuracy = accuracy_score(labels, pred)
+    # Evaluate clustering results
+    nmi_score = normalized_mutual_info_score(pred, labels)
+    ari_score = adjusted_rand_score(pred, labels)
 
+    # Print results
+    print(f'Method: {method}\nNMI: {nmi_score:.2f} \nARI: {ari_score:.2f}\n')
