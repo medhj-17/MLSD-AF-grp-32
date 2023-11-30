ea109a3789309db735c58fafff1f110c8a50f47b
mohamed.hjaji <mohamed.hjaji@gmail.com>
2023-11-30 10:31:43 +0100

    change main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.metrics.cluster import normalized_mutual_info_score, adjusted_rand_score
+from sentence_transformers import SentenceTransformer
+import numpy as np
 
+from sklearn.decomposition import FactorAnalysis
+from sklearn.cluster import KMeans
+from sklearn.datasets import fetch_20newsgroups
+
+# Define your custom functions
+def clust(embeddings, k):
+    kmeans = KMeans(n_clusters=k, random_state=42)
+    predictions = kmeans.fit_predict(embeddings)
+    return predictions
